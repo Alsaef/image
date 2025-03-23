c29fcb0a4762ee4fc07f71977afbbe853f1bee2a
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-03-23 15:18:12 +0600

    Update and rename object.js to mandata.js

--- react data/mandata.js
+++ react data/mandata.js
@@ -1,29 +1,5 @@
 const products = [
   {
-    category: "Woman",
-    type: "Jacket",
-    title: "Yellow Coat Jacket",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    price: 234,
-    image: "https://example.com/images/yellow-coat-woman.jpg"
-  },
-  {
-    category: "Woman",
-    type: "Jacket",
-    title: "Yellow Coat Jacket",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    price: 234,
-    image: "https://example.com/images/yellow-coat-woman2.jpg"
-  },
-  {
-    category: "Woman",
-    type: "Jacket",
-    title: "Yellow Coat Jacket",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    price: 234,
-    image: "https://example.com/images/yellow-coat-woman3.jpg"
-  },
-  {
     category: "Man",
     type: "Jacket",
     title: "Yellow Coat Jacket",
