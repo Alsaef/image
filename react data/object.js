768f40403f79f008fcf965959b2f376f0d4e3eb3
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-03-23 15:13:58 +0600

    Update and rename * to object.js

---
+++ react data/object.js
@@ -0,0 +1,50 @@
+const products = [
+  {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman.jpg"
+  },
+  {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman2.jpg"
+  },
+  {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman3.jpg"
+  },
+  {
+    category: "Man",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-man.jpg"
+  },
+  {
+    category: "Man",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-man2.jpg"
+  },
+  {
+    category: "Man",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-man3.jpg"
+  }
+];
