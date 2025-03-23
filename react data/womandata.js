865f5e1f257cd0e30ff424eae88d47a1c4232571
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-03-23 15:18:46 +0600

    Create womandata.js

---
+++ react data/womandata.js
@@ -0,0 +1,26 @@
+const products=[
+    {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman.jpg"
+  },
+  {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman2.jpg"
+  },
+  {
+    category: "Woman",
+    type: "Jacket",
+    title: "Yellow Coat Jacket",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    price: 234,
+    image: "https://example.com/images/yellow-coat-woman3.jpg"
+  }
+]
